daf85877bfbedc7af68ea1a7b7aee4976cfbfc71
rugved2802 <rugvedmairal28@gmail.com>
2024-07-16 01:40:38 +0530

    v2

--- screens/modalpassword.js
+++ screens/modalpassword.js
@@ -1,204 +1,44 @@
-import React, { useState } from 'react';
-import { View, StyleSheet, ScrollView } from 'react-native';
-import { TextInput, Button, Checkbox, Text, DefaultTheme, Provider as PaperProvider, IconButton } from 'react-native-paper';
-import PasswordChangeSuccessModal from './modalpassword';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Modal, Portal, Button, Text, IconButton } from 'react-native-paper';
 
-const theme = {
-  ...DefaultTheme,
-  colors: {
-    ...DefaultTheme.colors,
-    primary: '#0a84ff',
-  },
-};
-
-const ChangePasswordScreen = () => {
-  const [newPassword, setNewPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
-  const [passwordMatchError, setPasswordMatchError] = useState(false);
-  const [checked, setChecked] = useState(false);
-  const [showNewPassword, setShowNewPassword] = useState(false);
-  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
-  const [modalVisible, setModalVisible] = useState(false);
-
-  const handleCancelPress = () => {
-    console.log('Cancel button pressed!');
-    // Add functionality here
-  };
-
-  const handleSubmitPress = () => {
-    console.log('Submit button pressed!');
-    // Add functionality here
-    if (newPassword !== confirmPassword) {
-      setPasswordMatchError(true);
-
-    } else {
-      setPasswordMatchError(false);
-      setModalVisible(true);
-    }
-  };
-
-  const toggleShowNewPassword = () => {
-    setShowNewPassword(!showNewPassword);
-  };
-
-  const toggleShowConfirmPassword = () => {
-    setShowConfirmPassword(!showConfirmPassword);
-  };
-
-  const handleModalDismiss = () => {
-    setModalVisible(false);
-  };
- 
+const PasswordChangeSuccessModal = ({ visible, onDismiss }) => {
   return (
-    <PaperProvider theme={theme}>
-      <ScrollView contentContainerStyle={styles.container}>
-        <View style={styles.view}>
-          <Text style={styles.heading}>Change Your Password</Text>
+    <Portal>
+      <Modal visible={visible} onDismiss={onDismiss} contentContainerStyle={styles.modalContainer}>
+        <View style={styles.content}>
+          <IconButton icon="check-circle" iconColor="#4CAF50" size={60} />
+          <Text style={styles.text}>Password Change Successful!</Text>
+          <Button mode='contained' style={styles.button} onPress={onDismiss}>Go back to Login page</Button>
         </View>
-
-        <View style={styles.form}>
-          <Text style={styles.textinput}>New Password</Text>
-          <View style={styles.passwordContainer}>
-            <TextInput
-              secureTextEntry={!showNewPassword}
-              mode="flat"
-              style={styles.input}
-              value={newPassword}
-              onChangeText={setNewPassword}
-            />
-            <IconButton
-              icon={showNewPassword ? 'eye' : 'eye-off'}
-              onPress={toggleShowNewPassword}
-              style={styles.iconButton}
-            />
-          </View>
-
-          <Text style={styles.textinput}>Re-type Password</Text>
-          <View style={styles.passwordContainer}>
-            <TextInput
-              secureTextEntry={!showConfirmPassword}
-              mode="flat"
-              style={styles.input}
-              value={confirmPassword}
-              onChangeText={setConfirmPassword}
-            />
-            <IconButton
-              icon={showConfirmPassword ? 'eye' : 'eye-off'}
-              onPress={toggleShowConfirmPassword}
-              style={styles.iconButton}
-            />
-          </View>
-
-          {passwordMatchError && (
-            <Text style={styles.errorText}>Passwords do not match. Please try again.</Text>
-          )}
-
-          <View style={styles.checkboxcontainerLink}>
-            <Checkbox
-              status={checked ? 'checked' : 'unchecked'}
-              onPress={() => setChecked(!checked)}
-            />
-            <Text style={styles.tnc}>
-              I’ve read and agree with the <Text style={styles.link}>Terms and Conditions</Text> and the{' '}
-              <Text style={styles.link}>Privacy Policy</Text>
-            </Text>
-          </View>
-
-          <View style={styles.buttonContainer}>
-            <Button mode="text" onPress={handleCancelPress} style={styles.cancelButton}>
-              Cancel
-            </Button>
-            <Button mode="contained" onPress={handleSubmitPress} style={styles.submitButton} disabled={!checked} >
-              Submit
-            </Button>
-          </View>
-        </View>
-      
-        <PasswordChangeSuccessModal visible={modalVisible} onDismiss={handleModalDismiss} />
-
-      </ScrollView>
-    </PaperProvider>
+      </Modal>
+    </Portal>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flexGrow: 1,
-    padding: 20,
-    backgroundColor: '#fff',
-    paddingTop: 65,
-  },
-  view: {
-    marginTop: 40,
-  },
-  heading: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    marginBottom: 5,
-  },
-  form: {
-    marginTop: 20,
-  },
-  input: {
-    marginTop: 15,
-    marginBottom: 15,
-    height: 43,
-    flex: 1,
+  modalContainer: {
     backgroundColor: 'white',
-    marginLeft: 2,
-
-  },
-  textinput: {
-    marginBottom: 4,
-    fontWeight: 'bold',
-  },
-  passwordContainer: {
-    flexDirection: 'row',
+    padding: 20,
+    margin: 20,
+    justifyContent: 'center',
     alignItems: 'center',
-    marginBottom: 15,
-    height: 45,
-    borderWidth: 0.6,
-    borderRadius: 4,
+    borderRadius: 16,
+    elevation: 5,
   },
-  checkboxcontainerLink: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginBottom: 15,
-    marginRight: 40,
-  },
-  link: {
-    color: '#0a84ff',
-    fontWeight: 'bold',
-  },
-  buttonContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-  cancelButton: {
-    flex: 1,
-    marginRight: 10,
-    borderRadius: 5,
-    borderWidth: 1,
-    borderColor: '#ddd',
-  },
-  submitButton: {
-    flex: 1,
-    borderRadius: 5,
-  },
-  tnc: {
-    paddingTop: 12,
-    paddingBottom: 12,
-  },
-  errorText: {
-    color: 'red',
-    fontSize: 12,
-  },
-  iconButton: {
-    height: 30,
-    width: 30,
+  content: {
     justifyContent: 'center',
     alignItems: 'center',
   },
+  text: {
+    marginBottom: 15,
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  button: {
+    borderRadius: 8,
+    paddingHorizontal: 54,
+  },
 });
 
-export default ChangePasswordScreen;
+export default PasswordChangeSuccessModal;
